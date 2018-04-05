a48e7355e1ed2d6391d77d4062dd12cef1b028be
Tju\Zan Wang <wangzan@tju.edu.cn>
2018-04-05 23:09:24 +0800

    TestClassLoader

---
+++ adjavap/src/cn/tju/scs/c01/TestClassLoader.java
@@ -0,0 +1,35 @@
+package cn.tju.scs.c01;
+/**
+ * 
+ * @author Zan Wang
+ * 课前测试最后一道题，考察点：变量加载的顺序
+ *
+ */
+class Singleton {
+     private static Singleton singleton = new Singleton();
+     
+     public static int counter1;     
+     public static int counter2 = 0;
+     
+     private Singleton()
+     {
+            counter1++;
+            counter2++;
+     }
+     
+     public static Singleton getInstance()
+     {
+            return singleton;
+     }
+
+}
+
+public class TestClassLoader
+{
+     public static void main(String[] args) {
+           Singleton singleton = Singleton. getInstance();
+           System. out.println( "counter1 = " + singleton. counter1 );
+           System. out.println( "counter2 = " + singleton. counter2);
+     }
+}
+
